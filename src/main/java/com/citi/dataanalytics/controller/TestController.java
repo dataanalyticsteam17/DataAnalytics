ff8eb10871913bda98d88f2fcf6e3d7d59dd6716
Yellowcj <295930301@qq.com>
2019-08-09 12:55:44 +0800

    first

---
+++ src/main/java/com/citi/dataanalytics/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.citi.dataanalytics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "index";
+    }
+}
