9fd389f95703533f030a9796b37ed92846295859
Yellowcj <295930301@qq.com>
2019-08-11 20:04:44 +0800

    display list of stock(front-end and controller)

--- src/main/java/com/citi/dataanalytics/controller/TestController.java
+++ src/main/java/com/citi/dataanalytics/controller/TestController.java
@@ -1,7 +1,12 @@
 package com.citi.dataanalytics.controller;
 
+import com.citi.dataanalytics.entity.Stock;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TestController {
@@ -9,5 +14,19 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         return "index";
+    }
+
+    @RequestMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("/stock")
+    @ResponseBody
+    public List<Stock> getStocks(){
+        ArrayList<Stock> stocks = new ArrayList<>();
+        stocks.add(new Stock("20111111", "12:00", 42.54, 43.23, 345.5, 1234.35, 1, 2,3,4,5));
+        stocks.add(new Stock("20111112", "12:00", 42.54, 43.23, 345.5, 1234.35, 1, 2,3,4,5));
+        return stocks;
     }
 }
---
+++ src/main/java/com/citi/dataanalytics/entity/Stock.java
@@ -0,0 +1,117 @@
+package com.citi.dataanalytics.entity;
+
+public class Stock {
+    String date;
+    String time;
+    double open;
+    double high;
+    double low;
+    double close;
+    int volume;
+    int splitFactor;
+    int earnings;
+    int dividends;
+    int extrapolation;
+
+    public Stock(String date, String time, double open, double high, double low, double close, int volume, int splitFactor, int earnings, int dividends, int extrapolation) {
+        this.date = date;
+        this.time = time;
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+        this.volume = volume;
+        this.splitFactor = splitFactor;
+        this.earnings = earnings;
+        this.dividends = dividends;
+        this.extrapolation = extrapolation;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public double getOpen() {
+        return open;
+    }
+
+    public void setOpen(double open) {
+        this.open = open;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
+
+    public double getClose() {
+        return close;
+    }
+
+    public void setClose(double close) {
+        this.close = close;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getSplitFactor() {
+        return splitFactor;
+    }
+
+    public void setSplitFactor(int splitFactor) {
+        this.splitFactor = splitFactor;
+    }
+
+    public int getEarnings() {
+        return earnings;
+    }
+
+    public void setEarnings(int earnings) {
+        this.earnings = earnings;
+    }
+
+    public int getDividends() {
+        return dividends;
+    }
+
+    public void setDividends(int dividends) {
+        this.dividends = dividends;
+    }
+
+    public int getExtrapolation() {
+        return extrapolation;
+    }
+
+    public void setExtrapolation(int extrapolation) {
+        this.extrapolation = extrapolation;
+    }
+}
