c7d7dd439dddcc559936e11fb0249acbd7ddcb34
jw <leijiawei@hnu.edu.cn>
2019-08-12 12:30:41 +0800

    获取股票名称（从所有天数的文件夹里）

--- src/main/java/com/citi/dataanalytics/classes/GetStockName.java
+++ src/main/java/com/citi/dataanalytics/classes/GetStockName.java
@@ -1,32 +1,65 @@
 package com.citi.dataanalytics.classes;
+
 import java.io.File;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class GetStockName {
-//    public static void  main(String[] args )throws IOException, java.text.ParseException {
-//       // String ps= "C:\\Users\\jw\\Desktop\\DataAnalysis\\Data Analytics\\Test Data\\Quant Quote Market Data - Jan to Mar 2016\\allstocks_";
-//        String ps1= "C:\\Users\\jw\\Desktop\\DataAnalysis\\Data Analytics\\Test Data\\" +
-//                "Quant Quote Market Data - Jan to Mar 2016\\allstocks_20160104";
-//        ArrayList<String> files = new ArrayList<String>();
-//        getAllFileName(ps1, files);
+    public static void  main(String[] args )throws IOException, java.text.ParseException {
+//       String ps= "C:\\Users\\jw\\Desktop\\DataAnalysis\\Data Analytics\\Test Data\\Quant Quote Market Data - Jan to Mar 2016\\allstocks_";
+//       ArrayList<String> files = new ArrayList<String>();
+//       SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+//        Date start = sdf.parse("20160101");
+//        Date end = sdf.parse("20160630");
+//
+//
+//        while (start.before(end)) {
+//            String reStr = sdf.format(start);
+//            String filepath = ps + reStr ;
+//            File isfile = new File(filepath);
+//            if(isfile.exists()){
+//                getAllFileName(filepath, files);
+//            }
+//            Calendar ca = Calendar.getInstance();
+//            ca.setTime(start);
+//            ca.add(Calendar.DAY_OF_YEAR, 1);//day+1
+//            start = ca.getTime();
+//        }
+//         removeDuplicateWithOrder(files);
 //        getchar(files);
 //        for (String s:files){
 //            System.out.println(s);
 //        }
 //
-//       // removeDuplicateWithOrder(files);
-//
-//    }
-    public static ArrayList<String> getstock(){
-        String ps1= "C:\\Users\\jw\\Desktop\\DataAnalysis\\Data Analytics\\Test Data\\" +
-                "Quant Quote Market Data - Jan to Mar 2016\\allstocks_20160104";
-        ArrayList<String> files = new ArrayList<String>();
-        getAllFileName(ps1, files);
-        getchar(files);
-        for (String s:files){
-            System.out.println(s);
-        }
-        return files;
+    }
+    public static ArrayList<String> getstock()throws IOException, java.text.ParseException{
+            String ps= "C:\\Users\\jw\\Desktop\\DataAnalysis\\Data Analytics\\Test Data\\Quant Quote Market Data - Jan to Mar 2016\\allstocks_";
+            ArrayList<String> files = new ArrayList<String>();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+            Date start = sdf.parse("20160101");
+            Date end = sdf.parse("20160630");
+
+
+            while (start.before(end)) {
+                String reStr = sdf.format(start);
+                String filepath = ps + reStr ;
+                File isfile = new File(filepath);
+                if(isfile.exists()){
+                    getAllFileName(filepath, files);
+                }
+                Calendar ca = Calendar.getInstance();
+                ca.setTime(start);
+                ca.add(Calendar.DAY_OF_YEAR, 1);//day+1
+                start = ca.getTime();
+            }
+            removeDuplicateWithOrder(files);
+            getchar(files);
+            for (String s:files){
+                System.out.println(s);
+            }
+            return  files;
+
     }
     public static void getAllFileName(String path, ArrayList<String> fileNameList) {
 
@@ -44,18 +77,18 @@ public class GetStockName {
 
     }
     // 删除ArrayList中重复元素，保持顺序
-//    public static void removeDuplicateWithOrder(ArrayList list) {
-//        Set set = new HashSet();
-//        List newList = new ArrayList();
-//        for (Iterator iter = list.iterator(); iter.hasNext();) {
-//            Object element = iter.next();
-//            if (set.add(element))
-//                newList.add(element);
-//        }
-//        list.clear();
-//        list.addAll(newList);
-//        System.out.println( " remove duplicate " + list);
-//    }
+    public static void removeDuplicateWithOrder(ArrayList list) {
+        Set set = new HashSet();
+        List newList = new ArrayList();
+        for (Iterator iter = list.iterator(); iter.hasNext();) {
+            Object element = iter.next();
+            if (set.add(element))
+                newList.add(element);
+        }
+        list.clear();
+        list.addAll(newList);
+        System.out.println( " remove duplicate " + list);
+    }
     public static void getchar(ArrayList<String> fileNameList){
         ArrayList<String> newlist = new ArrayList();
         for(String s:fileNameList){
