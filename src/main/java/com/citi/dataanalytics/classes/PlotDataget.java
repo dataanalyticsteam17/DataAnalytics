fc6e2c9135462fd55f4dc41f2584d769795929ae
jw <leijiawei@hnu.edu.cn>
2019-08-13 13:35:47 +0800

    plot做图修正

--- src/main/java/com/citi/dataanalytics/classes/PlotDataget.java
+++ src/main/java/com/citi/dataanalytics/classes/PlotDataget.java
@@ -5,17 +5,16 @@ import java.util.ArrayList;
 
 public class PlotDataget {
 
+    public static void main(String[] args) throws IOException, java.text.ParseException{
+//        String value_name="close",start_time = "20160109",symbol = "a";
+//        String[][] result=getDataofday(value_name,start_time,symbol);
+//       System.out.println(result[0][0]+" "+result[0][1]);
 
-//    public static void main(String[] args) throws IOException, java.text.ParseException{
-////        String value_name="close",start_time = "20160104",symbol = "a";
-////        String[][] result=getDataofday(value_name,start_time,symbol);
-////       System.out.println(result[0][0]+" "+result[0][1]);
-//
-////        String value_name="close",start_time = "20160104",end_time ="20160108" ,symbol = "a";
-////        String[][] result=getDataoftime(value_name,start_time,end_time,symbol);
-////        System.out.println(result[result.length-1][0]+" "+result[result.length-1][1]);
-////        System.out.println(result.length);
-//    }
+        String value_name="Open Price",start_time = "20160104",end_time ="20160508" ,symbol = "a";
+        String[][] result=getDataoftime(value_name,start_time,end_time,symbol);
+        System.out.println(result[result.length-1][0]+" "+result[result.length-1][1]);
+        System.out.println(result.length);
+   }
 
     public static String[][] getDataofday(String value_name, String day,String symbol)throws IOException,java.text.ParseException{
         ArrayList<String> ls= Readdata.readData(day,day,symbol);
@@ -31,6 +30,7 @@ public class PlotDataget {
     }
     public static String[][] getDataoftime(String value_name,String start_date,String end_date,String symbol)throws IOException,java.text.ParseException{
         ArrayList<String> ls= Readdata.readData(start_date,end_date,symbol);
+        if (ls.isEmpty())return null;
         String[][] data = new String[ls.size()][2];
         for (int i=0;i<ls.size();i++){
             String[] str = ls.get(i).split(",");
@@ -42,14 +42,14 @@ public class PlotDataget {
     }
     public static int return_index(String value_name){
         switch (value_name){
-            case "openg" : return 2;
-            case "high" :return 3;
-            case "low":return 4;
-            case "close":return 5;
-            case  "volume":return 6;
-            case "split_factor": return 7;
-            case  "earning":return 8;
-            case "dividends" :return 9;
+            case "Open Price" : return 2;
+            case "High Price" :return 3;
+            case "Low Price":return 4;
+            case "Close Price":return 5;
+            case  "Volume":return 6;
+//            case "split_factor": return 7;
+//            case  "earning":return 8;
+//            case "dividends" :return 9;
             default:return -1;
         }
     }
