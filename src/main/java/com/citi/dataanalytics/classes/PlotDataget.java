b95fd1b55371fee8164ecf64df05c69ada55e8dc
jw <leijiawei@hnu.edu.cn>
2019-08-14 11:05:17 +0800

    统一调用作图的函数和返回值

--- src/main/java/com/citi/dataanalytics/classes/PlotDataget.java
+++ src/main/java/com/citi/dataanalytics/classes/PlotDataget.java
@@ -5,26 +5,33 @@ import java.util.ArrayList;
 
 public class PlotDataget {
 
-//    public static void main(String[] args) throws IOException, java.text.ParseException{
-////        String value_name="Close Price",start_time = "20160108",symbol = "a";
-////        String[][] result=getDataofday(value_name,start_time,symbol);
-////       System.out.println(result[0][0]+" "+result[0][1]);
-//
+    public static void main(String[] args) throws IOException, java.text.ParseException{
+        String value_name="Close Price",start_time = "20160108",symbol = "a";
+        String[][] result=getDataofday(value_name,start_time,symbol);
+       System.out.println(result[0][0]+" "+result[0][1]);
+
 //        String value_name="Open Price",start_time ="20160104",end_time = "20160104",symbol = "a";
 //        String[][] result=getDataoftime(value_name,start_time,end_time,symbol);
 //
 //        System.out.println(result[result.length-1][0]+" "+result[result.length-1][1]);
-//
-//   }
 
-    public static String[][] getDataofday(String value_name, String day,String symbol)throws IOException,java.text.ParseException{
+   }
+   public static String[][]getPlotData(String value_name, String start_date,String end_date,String symbol)
+           throws IOException,java.text.ParseException{
+        if(start_date.equals(end_date)) return getDataofday(value_name,start_date,symbol);
+        else return getDataoftime(value_name,start_date,end_date,symbol);
+
+   }
+
+    public static String[][] getDataofday(String value_name, String day,String symbol)
+            throws IOException,java.text.ParseException{
         ArrayList<String> ls= Readdata.readData(day,day,symbol);
         if (ls.isEmpty()) return null;
         String[][] data = new String[ls.size()][2];
 
         for (int i=0;i<ls.size();i++){
             String[] str = ls.get(i).split(",");
-            data[i][0]=Readdata.implet_4bit_time(str[1]);
+            data[i][0]=str[0]+Readdata.implet_4bit_time(str[1]);
             data[i][1]=str[return_index(value_name)];
 
         }
