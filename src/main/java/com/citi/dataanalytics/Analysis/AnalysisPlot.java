6ca69b4979cb9b579abab7e28d3caf02b80b30b2
jw <leijiawei@hnu.edu.cn>
2019-08-14 15:40:47 +0800

    统一作图调用接口

--- src/main/java/com/citi/dataanalytics/Analysis/AnalysisPlot.java
+++ src/main/java/com/citi/dataanalytics/Analysis/AnalysisPlot.java
@@ -3,18 +3,18 @@ package com.citi.dataanalytics.Analysis;
 import java.util.ArrayList;
 
 public class AnalysisPlot {
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+////        String start_time ="20160104",end_time = "20160304",symbol = "a";
+////        String[][] str = getPriceChange(start_time,end_time,symbol);
+////        for (int i=0;i<str.length;i++){
+////            System.out.println(str[i][0]+"  "+str[i][1]);
+////        }
 //        String start_time ="20160104",end_time = "20160304",symbol = "a";
-//        String[][] str = getPriceChange(start_time,end_time,symbol);
+//        String[][] str = getMaxMinChange(start_time,end_time,symbol);
 //        for (int i=0;i<str.length;i++){
 //            System.out.println(str[i][0]+"  "+str[i][1]);
 //        }
-        String start_time ="20160104",end_time = "20160304",symbol = "a";
-        String[][] str = getMaxMinChange(start_time,end_time,symbol);
-        for (int i=0;i<str.length;i++){
-            System.out.println(str[i][0]+"  "+str[i][1]);
-        }
-    }
+//    }
     public static String[][] getPriceChange(String start_date,String end_date,String symbol){
         ArrayList<AnalysisRecord> list = AnalysisOperation.get_analysisrecord(start_date,end_date,symbol);
         String[][] str = new String[list.size()][2];
